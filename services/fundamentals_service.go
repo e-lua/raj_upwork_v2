cdb844c307d344a48db64f7c0952414ffa4dbfa2
EdwardLopezLua <edward_lopez_cntcts@hotmail.com>
2022-04-15 16:31:04 -0500

    v1.80

--- services/fundamentals_service.go
+++ services/fundamentals_service.go
@@ -33,19 +33,20 @@ func AddAllData_Service(input_data Incoming_NewData_ToUploadAllData) (int, bool,
 			inco_newdata.Symbol = val.Symbol
 			inco_newdata.Api_token = input_data.Api_token
 
-			log.Println("--->>SYMBOL TO CHARGE: " + val.Symbol)
+			log.Println("--->>>>>>>>SYMBOL TO CHARGE: " + val.Symbol + " " + strconv.Itoa(input_data.Index) + "." + strconv.Itoa(counter))
 
 			_, boolerror, dataerror, _ := AddOneData_Service(inco_newdata)
 			if boolerror {
 				log.Println(val.Symbol, "result ? ", dataerror)
 			}
 
-			time.Sleep(10 * time.Second)
+			time.Sleep(15 * time.Second)
 
 			counter = counter + 1
 		}
 
 		if output_index == 1048 {
+			log.Println("****===============================END ALL===============================****")
 			break
 		}
 
@@ -99,11 +100,11 @@ func AddOneData_Service(input_data Incoming_NewData) (int, bool, string, string)
 	if error_get != nil {
 		return 403, true, "Internal error at the moment to get the data from TradableSymbols, details: " + error_get.Error(), ""
 	}
-	error_decode_respuesta_cp := json.NewDecoder(source_data.Body).Decode(&get_respuesta_companyprofile)
-	if error_decode_respuesta_cp != nil {
-		return 403, true, "Internal error at the moment to get the data from TradableSymbols, details: " + error_decode_respuesta_cp.Error(), ""
-	}
+	json.NewDecoder(source_data.Body).Decode(&get_respuesta_companyprofile)
+
 	log.Print("-------->Company profile-> extracted")
+
+	time.Sleep(1 * time.Second)
 
 	//Income Statement Annual
 	source_data_isa, error_get_isa := http.Get("https://fmpcloud.io/api/v3/income-statement/" + input_data.Symbol + "?limit=800000&apikey=" + input_data.Api_token)
@@ -112,12 +113,11 @@ func AddOneData_Service(input_data Incoming_NewData) (int, bool, string, string)
 		log.Println(source_data_isa)
 		return 403, true, "Internal error at the moment to get the data from Income Statement Annual, details: " + error_get_isa.Error(), ""
 	}
-	error_decode_respuesta_isa := json.NewDecoder(source_data_isa.Body).Decode(&get_respuesta_isa)
-	if error_decode_respuesta_isa != nil {
-		log.Println(source_data_isa)
-		return 403, true, "Internal error at the moment to get the data from Income Statement Annual, details: " + error_decode_respuesta_isa.Error(), ""
-	}
+	json.NewDecoder(source_data_isa.Body).Decode(&get_respuesta_isa)
+
 	log.Print("-------->Income Statement Annual-> extracted")
+
+	time.Sleep(1 * time.Second)
 
 	//Income Statement Quarter
 	source_data_isq, error_get_isq := http.Get("https://fmpcloud.io/api/v3/income-statement/" + input_data.Symbol + "?period=quarter&limit=800000&apikey=" + input_data.Api_token)
@@ -126,12 +126,11 @@ func AddOneData_Service(input_data Incoming_NewData) (int, bool, string, string)
 		log.Println(source_data_isq)
 		return 403, true, "Internal error at the moment to get the data from Income Statement Quarter, details: " + error_get_isq.Error(), ""
 	}
-	error_decode_respuesta := json.NewDecoder(source_data_isq.Body).Decode(&get_respuesta_isq)
-	if error_decode_respuesta != nil {
-		log.Println(source_data_isq)
-		return 403, true, "Internal error at the moment to get the data from Income Statement Quarter, details: " + error_get_isq.Error(), ""
-	}
+	json.NewDecoder(source_data_isq.Body).Decode(&get_respuesta_isq)
+
 	log.Print("-------->Income Statement Quarter-> extracted")
+
+	time.Sleep(1 * time.Second)
 
 	//Income Statement Annual Growth
 	source_data_isag, error_get_isag := http.Get("https://fmpcloud.io/api/v3/income-statement-growth/" + input_data.Symbol + "?limit=800000&apikey=" + input_data.Api_token)
@@ -139,11 +138,11 @@ func AddOneData_Service(input_data Incoming_NewData) (int, bool, string, string)
 	if error_get_isa != nil {
 		return 403, true, "Internal error at the moment to get the data from Income Statement Annual Growth, details: " + error_get_isag.Error(), ""
 	}
-	error_decode_respuesta_isag := json.NewDecoder(source_data_isag.Body).Decode(&get_respuesta_isa_isag)
-	if error_decode_respuesta_isag != nil {
-		return 403, true, "Internal error at the moment to get the data from Income Statement Annual Growth, details: " + error_decode_respuesta_isag.Error(), ""
-	}
+	json.NewDecoder(source_data_isag.Body).Decode(&get_respuesta_isa_isag)
+
 	log.Print("-------->Income Statement Annual Growth-> extracted")
+
+	time.Sleep(1 * time.Second)
 
 	//Income Statement Quarter Growth
 	source_data_isqg, error_get_isqg := http.Get("https://fmpcloud.io/api/v3/income-statement-growth/" + input_data.Symbol + "?period=quarter&limit=800000&apikey=" + input_data.Api_token)
@@ -151,11 +150,11 @@ func AddOneData_Service(input_data Incoming_NewData) (int, bool, string, string)
 	if error_get_isqg != nil {
 		return 403, true, "Internal error at the moment to get the data from Income Statement Quarter Growth, details: " + error_get_isqg.Error(), ""
 	}
-	error_decode_respuesta_isqg := json.NewDecoder(source_data_isqg.Body).Decode(&get_respuesta_isqg)
-	if error_decode_respuesta_isqg != nil {
-		return 403, true, "Internal error at the moment to get the data from Income Statement Quarter Growth, details: " + error_decode_respuesta_isqg.Error(), ""
-	}
+	json.NewDecoder(source_data_isqg.Body).Decode(&get_respuesta_isqg)
+
 	log.Print("-------->Income Statement Quarter Growth-> extracted")
+
+	time.Sleep(1 * time.Second)
 
 	//Balance Sheet Annual
 	source_data_bsa, error_get_bsa := http.Get("https://fmpcloud.io/api/v3/balance-sheet-statement/" + input_data.Symbol + "?limit=800000&apikey=" + input_data.Api_token)
@@ -163,11 +162,11 @@ func AddOneData_Service(input_data Incoming_NewData) (int, bool, string, string)
 	if error_get_isa != nil {
 		return 403, true, "Internal error at the moment to get the data from Balance Sheet Annual, details: " + error_get_bsa.Error(), ""
 	}
-	error_decode_respuesta_bsa := json.NewDecoder(source_data_bsa.Body).Decode(&get_respuesta_bsa)
-	if error_decode_respuesta_bsa != nil {
-		return 403, true, "Internal error at the moment to get the data from Balance Sheet Annual, details: " + error_decode_respuesta_bsa.Error(), ""
-	}
+	json.NewDecoder(source_data_bsa.Body).Decode(&get_respuesta_bsa)
+
 	log.Print("-------->Balance Sheet Annual-> extracted")
+
+	time.Sleep(1 * time.Second)
 
 	//Balance Sheet Quarter
 	source_data_bsq, error_get_bsq := http.Get("https://fmpcloud.io/api/v3/balance-sheet-statement/" + input_data.Symbol + "?period=quarter&limit=800000&apikey=" + input_data.Api_token)
@@ -175,11 +174,11 @@ func AddOneData_Service(input_data Incoming_NewData) (int, bool, string, string)
 	if error_get_bsq != nil {
 		return 403, true, "Internal error at the moment to get the data from Balance Sheet Quarter, details: " + error_get_bsq.Error(), ""
 	}
-	error_decode_respuesta_bsq := json.NewDecoder(source_data_bsq.Body).Decode(&get_respuesta_bsq)
-	if error_decode_respuesta_bsq != nil {
-		return 403, true, "Internal error at the moment to get the data from Balance Sheet Quarter, details: " + error_decode_respuesta_bsq.Error(), ""
-	}
+	json.NewDecoder(source_data_bsq.Body).Decode(&get_respuesta_bsq)
+
 	log.Print("-------->Balance Sheet Quarter-> extracted")
+
+	time.Sleep(1 * time.Second)
 
 	//Balance Sheet Annual Growth
 	source_data_bsag, error_get_bsag := http.Get("https://fmpcloud.io/api/v3/balance-sheet-statement-growth/" + input_data.Symbol + "?limit=800000&apikey=" + input_data.Api_token)
@@ -187,11 +186,11 @@ func AddOneData_Service(input_data Incoming_NewData) (int, bool, string, string)
 	if error_get_bsag != nil {
 		return 403, true, "Internal error at the moment to get the data from Balance Sheet Annual Growth, details: " + error_get_bsag.Error(), ""
 	}
-	error_decode_respuesta_bsag := json.NewDecoder(source_data_bsag.Body).Decode(&get_respuesta_bsag)
-	if error_decode_respuesta_bsag != nil {
-		return 403, true, "Internal error at the moment to get the data from Balance Sheet Annual Growth, details: " + error_decode_respuesta_bsag.Error(), ""
-	}
+	json.NewDecoder(source_data_bsag.Body).Decode(&get_respuesta_bsag)
+
 	log.Print("-------->Balance Sheet Annual Growth-> extracted")
+
+	time.Sleep(1 * time.Second)
 
 	//Balance Sheet Quarter Growth
 	source_data_bsqg, error_get_bsqg := http.Get("https://fmpcloud.io/api/v3/balance-sheet-statement-growth/" + input_data.Symbol + "?period=quarter&limit=800000&apikey=" + input_data.Api_token)
@@ -199,11 +198,11 @@ func AddOneData_Service(input_data Incoming_NewData) (int, bool, string, string)
 	if error_get_bsag != nil {
 		return 403, true, "Internal error at the moment to get the data from Balance Sheet Quarter Growth, details: " + error_get_bsqg.Error(), ""
 	}
-	error_decode_respuesta_bsqg := json.NewDecoder(source_data_bsqg.Body).Decode(&get_respuesta_bsqg)
-	if error_decode_respuesta_bsqg != nil {
-		return 403, true, "Internal error at the moment to get the data from Balance Sheet Quarter Growth, details: " + error_decode_respuesta_bsqg.Error(), ""
-	}
+	json.NewDecoder(source_data_bsqg.Body).Decode(&get_respuesta_bsqg)
+
 	log.Print("-------->Balance Sheet Quarter Growth-> extracted")
+
+	time.Sleep(1 * time.Second)
 
 	//Cash Flow Annual
 	source_data_cfa, error_get_cfa := http.Get("https://fmpcloud.io/api/v3/cash-flow-statement/" + input_data.Symbol + "?limit=800000&apikey=" + input_data.Api_token)
@@ -211,11 +210,11 @@ func AddOneData_Service(input_data Incoming_NewData) (int, bool, string, string)
 	if error_get_cfa != nil {
 		return 403, true, "Internal error at the moment to get the data from Cash Flow Annual, details: " + error_get_isa.Error(), ""
 	}
-	error_decode_respuesta_cfa := json.NewDecoder(source_data_cfa.Body).Decode(&get_respuesta_cfa)
-	if error_decode_respuesta_cfa != nil {
-		return 403, true, "Internal error at the moment to get the data from Cash Flow Annual, details: " + error_decode_respuesta_cfa.Error(), ""
-	}
+	json.NewDecoder(source_data_cfa.Body).Decode(&get_respuesta_cfa)
+
 	log.Print("-------->Cash Flow Annual-> extracted")
+
+	time.Sleep(1 * time.Second)
 
 	//Cash Flow Quarter
 	source_data_cfq, error_get_cfq := http.Get("https://fmpcloud.io/api/v3/cash-flow-statement/" + input_data.Symbol + "?period=quarter&limit=800000&apikey=" + input_data.Api_token)
@@ -223,11 +222,11 @@ func AddOneData_Service(input_data Incoming_NewData) (int, bool, string, string)
 	if error_get_cfq != nil {
 		return 403, true, "Internal error at the moment to get the data from Cash Flow Quarter, details: " + error_get_cfq.Error(), ""
 	}
-	error_decode_respuesta_cfq := json.NewDecoder(source_data_cfq.Body).Decode(&get_respuesta_cfq)
-	if error_decode_respuesta_cfq != nil {
-		return 403, true, "Internal error at the moment to get the data from Cash Flow Quarter, details: " + error_decode_respuesta_cfq.Error(), ""
-	}
+	json.NewDecoder(source_data_cfq.Body).Decode(&get_respuesta_cfq)
+
 	log.Print("-------->Cash Flow Quarter-> extracted")
+
+	time.Sleep(1 * time.Second)
 
 	//Cash Flow Annual Growth
 	source_data_cfag, error_get_cfag := http.Get("https://fmpcloud.io/api/v3/cash-flow-statement-growth/" + input_data.Symbol + "?limit=800000&apikey=" + input_data.Api_token)
@@ -235,11 +234,11 @@ func AddOneData_Service(input_data Incoming_NewData) (int, bool, string, string)
 	if error_get_cfag != nil {
 		return 403, true, "Internal error at the moment to get the data from Cash Flow Annual Growth, details: " + error_get_isa.Error(), ""
 	}
-	error_decode_respuesta_cfag := json.NewDecoder(source_data_cfag.Body).Decode(&get_respuesta_cfag)
-	if error_decode_respuesta_cfag != nil {
-		return 403, true, "Internal error at the moment to get the data from Cash Flow Annual Growth, details: " + error_decode_respuesta_cfag.Error(), ""
-	}
+	json.NewDecoder(source_data_cfag.Body).Decode(&get_respuesta_cfag)
+
 	log.Print("-------->Cash Flow Annual Growth-> extracted")
+
+	time.Sleep(1 * time.Second)
 
 	//Cash Flow Quarter Growth
 	source_data_cfqg, error_get_cfqg := http.Get("https://fmpcloud.io/api/v3/cash-flow-statement-growth/" + input_data.Symbol + "?period=quarter&limit=800000&apikey=" + input_data.Api_token)
@@ -247,11 +246,11 @@ func AddOneData_Service(input_data Incoming_NewData) (int, bool, string, string)
 	if error_get_cfqg != nil {
 		return 403, true, "Internal error at the moment to get the data from Cash Flow Quarter Growth, details: " + error_get_cfqg.Error(), ""
 	}
-	error_decode_respuesta_cfqg := json.NewDecoder(source_data_cfqg.Body).Decode(&get_respuesta_cfqg)
-	if error_decode_respuesta_cfqg != nil {
-		return 403, true, "Internal error at the moment to get the data from Cash Flow Quarter Growth, details: " + error_decode_respuesta_cfqg.Error(), ""
-	}
+	json.NewDecoder(source_data_cfqg.Body).Decode(&get_respuesta_cfqg)
+
 	log.Print("-------->Cash Flow Quarter Growth-> extracted")
+
+	time.Sleep(1 * time.Second)
 
 	//Financial Ratio Annual
 	source_data_fra, error_get_fra := http.Get("https://fmpcloud.io/api/v3/ratios/" + input_data.Symbol + "?limit=800000&apikey=" + input_data.Api_token)
@@ -259,11 +258,11 @@ func AddOneData_Service(input_data Incoming_NewData) (int, bool, string, string)
 	if error_get_fra != nil {
 		return 403, true, "Internal error at the moment to get the data from Financial Ratio Annual, details: " + error_get_fra.Error(), ""
 	}
-	error_decode_respuesta_fra := json.NewDecoder(source_data_fra.Body).Decode(&get_respuesta_fra)
-	if error_decode_respuesta_fra != nil {
-		return 403, true, "Internal error at the moment to get the data from Financial Ratio Annual, details: " + error_decode_respuesta_fra.Error(), ""
-	}
+	json.NewDecoder(source_data_fra.Body).Decode(&get_respuesta_fra)
+
 	log.Print("-------->Financial Ratio Annual-> extracted")
+
+	time.Sleep(1 * time.Second)
 
 	//Financial Ratio Quarter
 	source_data_frq, error_get_frq := http.Get("https://fmpcloud.io/api/v3/ratios/" + input_data.Symbol + "?period=quarter&limit=800000&apikey=" + input_data.Api_token)
@@ -271,11 +270,11 @@ func AddOneData_Service(input_data Incoming_NewData) (int, bool, string, string)
 	if error_get_frq != nil {
 		return 403, true, "Internal error at the moment to get the data from Financial Ratio Quarter, details: " + error_get_frq.Error(), ""
 	}
-	error_decode_respuesta_frq := json.NewDecoder(source_data_frq.Body).Decode(&get_respuesta_frq)
-	if error_decode_respuesta_frq != nil {
-		return 403, true, "Internal error at the moment to get the data from Financial Ratio Quarter, details: " + error_decode_respuesta_frq.Error(), ""
-	}
+	json.NewDecoder(source_data_frq.Body).Decode(&get_respuesta_frq)
+
 	log.Print("-------->Financial Ratio Quarter-> extracted")
+
+	time.Sleep(1 * time.Second)
 
 	//Financial Ratio AnnualTTM
 	source_data_frattm, error_get_frattm := http.Get("https://fmpcloud.io/api/v3/ratios-ttm/" + input_data.Symbol + "?apikey=" + input_data.Api_token)
@@ -283,11 +282,11 @@ func AddOneData_Service(input_data Incoming_NewData) (int, bool, string, string)
 	if error_get_frattm != nil {
 		return 403, true, "Internal error at the moment to get the data from Financial Ratio Annual, details: " + error_get_frattm.Error(), ""
 	}
-	error_decode_respuesta_frattm := json.NewDecoder(source_data_frattm.Body).Decode(&get_respuesta_frattm)
-	if error_decode_respuesta_frattm != nil {
-		return 403, true, "Internal error at the moment to get the data from Financial Ratio Annual, details: " + error_decode_respuesta_frattm.Error(), ""
-	}
+	json.NewDecoder(source_data_frattm.Body).Decode(&get_respuesta_frattm)
+
 	log.Print("-------->Financial Ratio Annual-> extracted")
+
+	time.Sleep(1 * time.Second)
 
 	//Key Metrics Company TTM
 	source_data_kmcttm, error_get_kmcttm := http.Get("https://fmpcloud.io/api/v3/key-metrics-ttm/" + input_data.Symbol + "?limit=800000&apikey=" + input_data.Api_token)
@@ -295,11 +294,11 @@ func AddOneData_Service(input_data Incoming_NewData) (int, bool, string, string)
 	if error_get_kmcttm != nil {
 		return 403, true, "Internal error at the moment to get the data from Key Metrics Company TTM, details: " + error_get_kmcttm.Error(), ""
 	}
-	error_decode_respuesta_kmcttm := json.NewDecoder(source_data_kmcttm.Body).Decode(&get_respuesta_kmcttm)
-	if error_decode_respuesta_kmcttm != nil {
-		return 403, true, "Internal error at the moment to get the data from Key Metrics Company TTM, details: " + error_decode_respuesta_kmcttm.Error(), ""
-	}
+	json.NewDecoder(source_data_kmcttm.Body).Decode(&get_respuesta_kmcttm)
+
 	log.Print("-------->Key Metrics Company TTM-> extracted")
+
+	time.Sleep(1 * time.Second)
 
 	//Key Metrics Annual
 	source_data_kma, error_get_kma := http.Get("https://fmpcloud.io/api/v3/key-metrics/" + input_data.Symbol + "?limit=800000&apikey=" + input_data.Api_token)
@@ -307,11 +306,11 @@ func AddOneData_Service(input_data Incoming_NewData) (int, bool, string, string)
 	if error_get_kma != nil {
 		return 403, true, "Internal error at the moment to get the data from Key Metrics Annual, details: " + error_get_kma.Error(), ""
 	}
-	error_decode_respuesta_kma := json.NewDecoder(source_data_kma.Body).Decode(&get_respuesta_kma)
-	if error_decode_respuesta_kma != nil {
-		return 403, true, "Internal error at the moment to get the data from Key Metrics Annual, details: " + error_decode_respuesta_kma.Error(), ""
-	}
+	json.NewDecoder(source_data_kma.Body).Decode(&get_respuesta_kma)
+
 	log.Print("-------->Key Metrics Annual-> extracted")
+
+	time.Sleep(1 * time.Second)
 
 	//Key Metrics Quarter
 	source_data_kmq, error_get_kmq := http.Get("https://fmpcloud.io/api/v3/key-metrics/" + input_data.Symbol + "?period=quarter&limit=800000&apikey=" + input_data.Api_token)
@@ -319,11 +318,11 @@ func AddOneData_Service(input_data Incoming_NewData) (int, bool, string, string)
 	if error_get_kmq != nil {
 		return 403, true, "Internal error at the moment to get the data from Key Metrics Quarter, details: " + error_get_kmq.Error(), ""
 	}
-	error_decode_respuesta_kmq := json.NewDecoder(source_data_kmq.Body).Decode(&get_respuesta_kmq)
-	if error_decode_respuesta_kmq != nil {
-		return 403, true, "Internal error at the moment to get the data from Key Metrics Quarter, details: " + error_decode_respuesta_kmq.Error(), ""
-	}
+	json.NewDecoder(source_data_kmq.Body).Decode(&get_respuesta_kmq)
+
 	log.Print("-------->Key Metrics Quarter-> extracted")
+	time.Sleep(1 * time.Second)
+
 	log.Print("-------->SUCCESSFUL EXTRACTION DATA PROCESS")
 
 	error_add := all.Si_Add(input_data.Symbol, get_respuesta_companyprofile, get_respuesta_isa, get_respuesta_isq, get_respuesta_isa_isag, get_respuesta_isqg, get_respuesta_bsa, get_respuesta_bsq, get_respuesta_bsag, get_respuesta_bsqg, get_respuesta_cfa, get_respuesta_cfq, get_respuesta_cfag, get_respuesta_cfqg, get_respuesta_fra, get_respuesta_frq, get_respuesta_frattm, get_respuesta_kmcttm, get_respuesta_kma, get_respuesta_kmq)
